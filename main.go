68e8ce118d7f53bd4c1688485de9c1846908ee09
Geoff 👌  Cowan <geoffc@uber.com>
2020-04-30 02:54:49 +0000

    fixing devenv

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	app := tview.NewApplication()
+
+	flex := tview.NewFlex().
+		AddItem( tview.NewInputField().
+		SetLabel("Enter a number: ").
+		SetPlaceholder("E.g. 1234").
+		SetFieldWidth(10).
+		SetAcceptanceFunc(tview.InputFieldInteger).
+		SetDoneFunc(func(key tcell.Key) {
+			app.Stop()
+		})
+	).
+	AddItem(tview.NewTextView().
+		Set
+	box := tview.NewBox().SetBorder(true).SetTitle("siv")
+
+	inputField :=
+
 	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
 		panic(err)
 	}
